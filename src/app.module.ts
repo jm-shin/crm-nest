6d31b464f3e7184b1611cae0535da4d1db209103
jongmin <ace4463@gmail.com>
2022-04-19 17:26:46 +0900

    build: update env host

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,7 +41,7 @@ const transports = {
     TypeOrmModule.forRoot({
       name: 'default',
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
